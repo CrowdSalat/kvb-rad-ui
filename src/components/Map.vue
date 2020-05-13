5213921ac0fb9494961c4f56a4b79483ec7c4148
crowdsalat <janweyrich93@gmail.com>
2020-05-13 21:08:23 +0200

    add leaftlet.heat to add a heatmap layer

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,17 +5,34 @@
 <script>
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+// need to be imported this way or the library will not get loaded properly
+import 'leaflet.heat';
 
 export default {
   name: 'Map',
   data() {
     return {
       map: null,
+      heatLayer: null,
     };
   },
   mounted() {
     this.initMap();
     this.initLayer();
+    const intensity = 1.0;
+    this.initHeatmapLayer([
+      [50.935173, 6.953101, intensity],
+      [50.915173, 6.953102, intensity],
+      [50.935173, 6.953103, intensity],
+      [50.925173, 6.953104, intensity],
+      [50.935173, 6.953105, intensity],
+      [50.935173, 6.953106, intensity],
+      [50.935173, 6.953107, intensity],
+      [50.935173, 6.953108, intensity],
+      [50.945173, 6.953109, intensity],
+      [50.935173, 6.953100, intensity],
+      [50.935171, 6.953101, intensity],
+    ]);
   },
   beforeDestroy() {
     this.removeMap();
@@ -23,7 +40,7 @@ export default {
   methods: {
     initMap() {
       this.map = L.map('mapContainer')
-        .setView([50.935173, 6.953101], 12);
+        .setView([50.935173, 6.953101], 14);
     },
     initLayer() {
       L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -31,6 +48,13 @@ export default {
           '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       })
         .addTo(this.map);
+    },
+    initHeatmapLayer(positions) {
+      if (this.heatLayer) {
+        this.map.removeLayer(this.heatLayer);
+      }
+      this.heatLayer = L.heatLayer(positions, { radius: 25 });
+      this.heatLayer.addTo(this.map);
     },
     removeMap() {
       if (this.map) {
