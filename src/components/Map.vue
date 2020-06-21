5d5fccd21da0624f34c450be8dd636c25db0083e
Jan Weyrich <janweyrich93@gmail.com>
2020-06-21 10:02:38 +0200

    add draw precedence (red over orange over green)

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -98,6 +98,10 @@ export default {
       });
       let count = 0;
       let lastTour = copyWaypoint[0];
+      const drawPrecendence = [];
+      drawPrecendence.green = [];
+      drawPrecendence.orange = [];
+      drawPrecendence.red = [];
       copyWaypoint.forEach((tour) => {
         if (tour[0][0] === lastTour[0][0]
           && tour[0][1] === lastTour[0][1]
@@ -108,12 +112,15 @@ export default {
           const color = this.getColor(count);
           const polyline = L.polyline(lastTour.slice(), { color });
           polyline.bindPopup(`Used ${count} times.`);
-          polyline.addTo(this.map);
+          drawPrecendence[color].push(polyline);
           count = 1;
         }
         // eslint-disable-next-line prefer-destructuring
         lastTour = tour;
       });
+      drawPrecendence.green.forEach((lines) => lines.addTo(this.map));
+      drawPrecendence.orange.forEach((lines) => lines.addTo(this.map));
+      drawPrecendence.red.forEach((lines) => lines.addTo(this.map));
     },
     getColor(count) {
       if (count < this.yellowThreshold) {
