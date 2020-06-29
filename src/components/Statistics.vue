539e2e03c7c9e4222b06caf43365f5f7e5d52c89
Jan Weyrich <janweyrich93@gmail.com>
2020-06-29 18:25:21 +0200

    changed the looks of the statistic

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -4,82 +4,87 @@
     <v-dialog
       style="z-index: 1500;"
       v-model="overlay"
+      max-width="1500px"
     >
-      <v-container>
-        <v-row>
-          <v-col sm="4">
-            <v-card
-              class="mx-auto"
-              max-width="400"
-              height="380px"
-              padding>
-              <v-sparkline
-                :value="rentalHours"
-                :labels="rentalHoursLabel"
-                color="black"
-                line-width="2"
-                padding="16"
-                size="200px"
-              ></v-sparkline>
-              <v-card-title>Rented bikes per hour.</v-card-title>
-              <v-simple-table dense
-                              height="170px">
-                <template v-slot:default>
-                  <thead>
-                  <tr>
-                    <th class="text-left">Hour</th>
-                    <th class="text-left">Rented bikes</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr v-for="(item, index) in rentalHours" :key="index">
-                    <td>{{ index }}</td>
-                    <td>{{ item }}</td>
-                  </tr>
-                  </tbody>
-                </template>
-              </v-simple-table>
-            </v-card>
-          </v-col>
-          <v-col sm="4">
-            <v-card
-              class="mx-auto"
-              max-width="400"
-              height="380px">
-              <v-icon
-                size="200px"
-              >mdi-bike
-              </v-icon>
-              <v-card-title>Rented bikes in the last 24 hours</v-card-title>
-              <v-card-text class="headline font-weight-bold" center>
-                <div>{{rentedBikes24h}}</div>
-              </v-card-text>
-            </v-card>
-          </v-col>
-          <v-col sm="4">
-            <v-card
-              class="mx-auto"
-              max-width="400"
-              height="380px">
-              <v-icon
-                size="200px"
-              >mdi-ruler
-              </v-icon>
-              <v-card-title>Travelled KM in the last 24 hours</v-card-title>
-              <v-card-text class="headline font-weight-bold" center>
-                <div>{{riddenDistance24hKm}}</div>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-        <v-btn
-          color="orange"
-          text
-          @click="toggleStatsOverlay">
-          Close
-        </v-btn>
-
-      </v-container>
+      <v-sheet color="secondary">
+        <v-container>
+          <v-row dense>
+            <v-col sm="4">
+              <v-card
+                class="mx-auto"
+                max-width="400"
+                height="380px"
+                padding
+              >
+                <v-sparkline
+                  :value="rentalHours"
+                  :labels="rentalHoursLabel"
+                  color="black"
+                  line-width="2"
+                  padding="16"
+                  size="200px"
+                ></v-sparkline>
+                <v-card-title>Rented bikes per hour.</v-card-title>
+                <v-simple-table dense
+                                height="170px">
+                  <template v-slot:default>
+                    <thead>
+                    <tr>
+                      <th class="text-left">Hour</th>
+                      <th class="text-left">Rented bikes</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="(item, index) in rentalHours" :key="index">
+                      <td>{{ index }}</td>
+                      <td>{{ item }}</td>
+                    </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
+              </v-card>
+            </v-col>
+            <v-col sm="4">
+              <v-card
+                class="mx-auto"
+                max-width="400"
+                height="380px"
+              >
+                <v-icon
+                  size="200px"
+                >mdi-bike
+                </v-icon>
+                <v-card-title>Rented bikes in the last 24 hours</v-card-title>
+                <v-card-text class="headline font-weight-bold" center>
+                  <div>{{rentedBikes24h}}</div>
+                </v-card-text>
+              </v-card>
+            </v-col>
+            <v-col sm="4">
+              <v-card
+                class="mx-auto"
+                max-width="400"
+                height="380px"
+              >
+                <v-icon
+                  size="200px"
+                >mdi-ruler
+                </v-icon>
+                <v-card-title>Travelled KM in the last 24 hours</v-card-title>
+                <v-card-text class="headline font-weight-bold" center>
+                  <div>{{riddenDistance24hKm}}</div>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-btn
+            color="accent"
+            text
+            @click="toggleStatsOverlay">
+            Close
+          </v-btn>
+        </v-container>
+      </v-sheet>
     </v-dialog>
   </section>
 </template>
@@ -90,6 +95,7 @@ export default {
   data() {
     return {
       overlay: false,
+      cardColor: 'primary',
       rentalHours: [],
       rentalHoursLabel: [...Array(24)
         .keys()],
